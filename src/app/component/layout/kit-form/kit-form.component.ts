a521334f4616f6f280cbf1a4ab7e6a069c3d34d1
Priyadarshini Gopal <priyadarshini.gopal@ni.com>
2024-03-24 20:46:41 +0530

    kit form for standard

--- src/app/component/layout/kit-form/kit-form.component.ts
+++ src/app/component/layout/kit-form/kit-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
@@ -7,25 +7,41 @@ import { MatDialogRef } from '@angular/material/dialog';
   templateUrl: './kit-form.component.html',
   styleUrls: ['./kit-form.component.css']
 })
-export class KitFormComponent {
+export class KitFormComponent implements OnInit {
   @Input() public kitType: 'none' | 'standard' | 'elite' = 'none';
-  kitForm: FormGroup;
-  phoneNumberInvalid = false;
-  addressInvalid = false;
-  kitDetails = "";
-  kitPrice = 0;
+  @Input() public description: { [key: string]: string } = {};
+  @Input() public price = 0;
+  @Input() public imageUrls: string[] = [];
+
+  standardForm: FormGroup;
+  currentImageIndex = 0;
+  totalPrice = 0;
 
   constructor(private dialogRef: MatDialogRef<KitFormComponent>, private formBuilder: FormBuilder) {
-    this.kitForm = this.formBuilder.group({
-      phoneNumber: ['', [Validators.required, Validators.pattern('[0-9]*')]],
-      address: ['', Validators.required]
+    this.standardForm = this.formBuilder.group({
+      quantity: [1, [Validators.required]],
+      price: [],
+      phoneNumber: [null, [Validators.required, Validators.pattern('[0-9]*')]],
+      address: this.formBuilder.group({
+        doorNumber: [null, [Validators.required]],
+        street: [null, [Validators.required]],
+        city: [null, [Validators.required]],
+        state: [null, [Validators.required]],
+        landmark: [null],
+        zip: [null, [Validators.required, Validators.pattern('[0-9]*')]]
+      })
     });
   }
 
+  ngOnInit(): void {
+    this.standardForm.get('description')?.setValue(this.description);
+    this.standardForm.get('price')?.setValue(this.price);
+    this.totalPrice = this.price;
+  }
+
   submit() {
-    if (this.kitForm.invalid) {
-      this.phoneNumberInvalid = this.kitForm.get('phoneNumber')?.invalid ?? false;
-      this.addressInvalid = this.kitForm.get('address')?.invalid ?? false;
+    if (this.standardForm.invalid) {
+      this.standardForm.markAllAsTouched();
     } else {
       this.dialogRef.close();
     }
@@ -33,5 +49,44 @@ export class KitFormComponent {
 
   closeForm() {
     this.dialogRef.close();
+  }
+
+  get currentImage(): string {
+    return this.imageUrls[this.currentImageIndex];
+  }
+
+  previousImage(): void {
+    if (this.currentImageIndex > 0) {
+      this.currentImageIndex--;
+    } else {
+      this.currentImageIndex = this.imageUrls.length - 1; // Loop back to the last image
+    }
+  }
+
+  nextImage(): void {
+    if (this.currentImageIndex < this.imageUrls.length - 1) {
+      this.currentImageIndex++;
+    } else {
+      this.currentImageIndex = 0;
+    }
+  }
+
+  calculatePrice(): void {
+    const quantity = this.standardForm.get('quantity')?.value;
+    this.totalPrice = (quantity > 0 ? quantity : 0) * this.price;
+  }
+
+  controlInvalid(controlName: string) {
+    const control = this.getFormControl(controlName);
+    return control?.touched && control?.invalid;
+  }
+  
+  private getFormControl(controlName: string) {
+    if (this.kitType === 'standard') {
+      return this.standardForm.get(controlName);
+    } else if (this.kitType === 'elite') {
+      return this.standardForm.get(controlName);
+    }
+    return null;
   }
 }
--- src/app/component/layout/kit-pop-up/kit-pop-up.component.ts
+++ src/app/component/layout/kit-pop-up/kit-pop-up.component.ts
@@ -7,7 +7,39 @@ import { Component } from '@angular/core';
 })
 export class KitPopUpComponent {
   kitType: 'none' | 'standard' | 'elite' = 'none';
+  imageUrls: string[] = [];
+  description: { [key: string]: string } = {};
+  price = 0;
+  
   setKitType(type: 'standard' | 'elite') {
+    if (type === 'standard') {
+      this.setStandardKitDetails();
+    } else {
+      this.setEliteKitDetails();
+
+    }
+
     this.kitType = type;
+  }
+
+  private setStandardKitDetails() {
+    this.imageUrls = ['assets/standard-kit/chess-board-mat.jpg', 'assets/standard-kit/mat-and-coins.jpg', 'assets/standard-kit/mat-and-coins-full-view.jpg'];
+    this.description = {
+      Material: 'Vinyl',
+      Colour: 'Green and White',
+      'Item Weight': '150 g',
+      'Board Dimension': '17 X 17 inches',
+      'Coins Dimension': '5 X 5 X 5 cm'
+      };
+    this.price = 50;
+  }
+
+  private setEliteKitDetails() {
+    this.imageUrls = ['assets/elite-chess-board.jpg', 'assets/elite-chess-pieces.jpg', 'assets/elite-chess-clock.jpg', 'assets/elite-chess.jpg'];
+    this.description = {
+      board: 'A standard chess board',
+      coins: 'A set of chess coins'
+    };
+    this.price = 100;
   }
 }
